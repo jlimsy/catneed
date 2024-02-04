9f983ae7552a2effc95b74919c869d84ba102860
jlimsy <limsy.joey@gmail.com>
2024-02-04 09:17:58 +0800

    install dotenv and mongoose

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+//* Database
+require("dotenv").config();
+require("./config/database");
 
 const app = express();
 
