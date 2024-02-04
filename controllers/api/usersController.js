a9dfb16ff5482677655c2f7cf3e242c0e682a650
jlimsy <limsy.joey@gmail.com>
2024-02-04 20:34:54 +0800

    create jwt token

--- controllers/api/usersController.js
+++ controllers/api/usersController.js
@@ -1,10 +1,27 @@
-module.exports = { create };
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
 
-function create(req, res) {
-  res.json({
-    user: {
-      username: req.body.username,
-      email: req.body.email,
-    },
-  });
+async function create(req, res) {
+  try {
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    console.log(token);
+
+    res.json(token);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 }
+
+/*-- Helper Functions --*/
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
+}
+
+module.exports = { create };
