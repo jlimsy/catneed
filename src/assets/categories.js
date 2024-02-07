b1a881c268ecc1fa050575d557375fa018d3680c
jlimsy <limsy.joey@gmail.com>
2024-02-07 16:31:00 +0800

    define categories

---
+++ src/assets/categories.js
@@ -0,0 +1,11 @@
+const categories = {
+  bedding: "Bedding and Furniture",
+  food: "Food and Treats",
+  grooming: "Grooming Supplies",
+  medical: "Medical Supplies",
+  safety: "Safety and Enclosures",
+  toys: "Toys and Enrichment",
+  others: "Others",
+};
+
+export default categories;
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import categories from "../assets/categories";
 
 const BASE_URL = "/api/image/upload";
 
@@ -50,6 +51,17 @@ export default function PostForm() {
     <section>
       <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
         <div className="mb-5">
+          <label htmlFor="name" className="block mb-2">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="block border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        <div className="mb-5">
           <label htmlFor="photo">Upload photo of your item:</label>
 
           <input
@@ -64,42 +76,19 @@ export default function PostForm() {
 
         <div className="mb-5">
           <label
-            htmlFor="large-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="category"
+            className="block mb-2 font-medium text-gray-900"
           >
-            Large input
+            Category:
           </label>
-          <input
-            type="text"
-            id="large-input"
-            className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="base-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          <select
+            id="countries"
+            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           >
-            Base input
-          </label>
-          <input
-            type="text"
-            id="base-input"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="small-input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Small input
-          </label>
-          <input
-            type="text"
-            id="small-input"
-            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
+            {Object.keys(categories).map((category) => (
+              <option key={category}> {categories[category]} </option>
+            ))}
+          </select>
         </div>
       </form>
     </section>
